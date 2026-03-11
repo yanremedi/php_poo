9ec93594a26829444d73818be0af28f549aa9c52
yanremedi <yan.r.silva8@aluno.senai.br>
2026-03-11 11:31:47 -0300

    atualizei

--- calculadora_imc_poo.php
+++ calculadora_imc_poo.php
@@ -1,10 +1,10 @@
 <?php
 
-function calcularIMC($peso, $altura) {
+function calcularIMC($peso, $altura, $nome) {
     return $peso / ($altura * $altura);
 }
 
-$peso = 77; $altura = 1.85;
+$peso = 77; $altura = 1.85; $nome = "João";
 
 function classificarIMC($imc) {
     if ($imc < 18.5) {
@@ -18,10 +18,11 @@ function classificarIMC($imc) {
     }
 }
 
-$imc = calcularIMC($peso, $altura);
+$imc = calcularIMC($peso, $altura, $nome);
 $classificacao = classificarIMC($imc);
 
-echo "Peso: $peso kg\n";
-echo "Altura: $altura m\n";
-echo "IMC: " . number_format($imc, 2) . "\n";
+echo "Nome: $nome \n" . "<br>";
+echo "Peso: $peso kg\n" ."<br>";
+echo "Altura: $altura m\n". "<br>";
+echo "IMC: " . number_format($imc, 2) . "\n" ."<br>";
 echo "Classificação: $classificacao";
