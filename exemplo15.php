a928d33aa385f55e8a813d6216b6d5705f783c4f
yanremedi <yan.r.silva8@aluno.senai.br>
2026-04-15 09:05:02 -0300

    feitoo o ex15

---
+++ exemplo15.php
@@ -0,0 +1,63 @@
+<?php
+class Conta
+{
+    public $numero;
+    public $saldo;
+
+    function __construct($numero, $saldo)
+    {
+        $this->numero = $numero;
+        $this->saldo = $saldo;
+    }
+
+    function creditar($valor)
+    {
+        $this->saldo = $this->saldo + $valor;
+    }
+
+    function debitar($valor)
+    {
+        $this->saldo = $this->saldo - $valor;
+    }
+
+    function transferir($outraConta, $valor)
+    {
+        if ($this->saldo >= $valor) {
+            $this->debitar($valor);
+            $outraConta->creditar($valor);
+        }
+    }
+}
+
+class Poupanca extends Conta
+{
+    public $juros;
+
+    function __construct($numero, $saldo, $juros)
+    {
+        parent::__construct($numero, $saldo);
+        $this->juros = $juros;
+    }
+
+    function creditar($valor)
+    {
+        parent::creditar($valor);
+        $this->atualizarJuros(); 
+    }
+
+    function atualizarJuros()
+    {
+        $this->saldo = $this->saldo * (1 + $this->juros);
+    }
+}
+
+$conta = new Conta(1, 150);
+$conta->creditar(50);
+$conta->debitar(100);
+echo "Saldo da conta $conta->numero: $conta->saldo <br>";
+
+$poupanca = new Poupanca(2, 150, 0.10);
+$poupanca->creditar(50);
+$poupanca->debitar(100);
+$poupanca->atualizarJuros();
+echo "Saldo da Poupança $poupanca->numero: $poupanca->saldo <br>";
